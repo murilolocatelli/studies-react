91bb8b148a4f4964436535ff30018d25e77ed21b
murilolocatelli <murilolocatelli@gmail.com>
2022-12-01 08:19:29 -0300

    .

--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -5,5 +5,21 @@ module.exports = env => {
       output: {
         filename: 'bundle.js',
       },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'babel-loader',
+                options: {
+                  cacheDirectory: true,
+                },
+              },
+            ],
+          },
+        ],
+      }
     }
   }
